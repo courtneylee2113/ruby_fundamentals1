401a86e2ebf0989e53cbd90a16860832e63ac28d
Courtney Lee <courtneylee2113@gmail.com>
2015-03-31 22:37:45 -0400

    see julie about assignment four.

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+a=45628
+b=7839
+puts "the value of a is #{a}."
+puts "The value of b is #{b}"
+puts " The value of a mulitplied by b is #{a*b}."
+
+
+(true && false) || (false && true) || !(false && false)
